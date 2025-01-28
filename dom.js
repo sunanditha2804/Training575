df7d25bbf174b6c125e8475613deba97612fb481
sunanditha2804 <2211cs010575@mallareddyuniversity.ac.in>
2025-01-28 22:41:22 +0530

    Done

--- dom.js
+++ dom.js
@@ -206,15 +206,45 @@ let form = document.querySelector("form");
 
 let username = document.getElementById("uName");
 let password = document.getElementById("uPass");
+let check = document.getElementById("check");
+let show = document.getElementById("show")
 
 let gender = document.getElementsByName("gender");
-console.log(gender);    
+
+// ! hiding and showing the password values
+check.addEventListener("click",event=>{
+    // console.log(event.target.checked);
+    if(event.target.checked == true){
+        password.setAttribute("type" , "text");
+        show.innerText = "hide password";
+    }else{
+        password.setAttribute("type" , "password");
+        show.innerText = "show password";
+    }
+})
 
 form.addEventListener("submit" , event=>{
     event.preventDefault();
     let un = username.value;
     let up = password.value;
-    let gen = gender.value;
+    let gen = "";
 
-    console.log(un , up , gen);
+    for(let i=0;i<=gender.length-1;i++){
+        // console.log(gender[i].value);
+        // console.log(gender[i].checked);
+        if(gender[i].checked == true){
+            // console.log(gender[i].value);
+            gen = gender[i].value;
+        }
+    }
+
+    // console.log(un , up , gen);
+    let userDetails={
+        username:un,
+        password:up,
+        gender:gen
+    }
+    console.log(userDetails);
+    sessionStorage.setItem("userData" ,
+         JSON.stringify(userDetails))
 })
